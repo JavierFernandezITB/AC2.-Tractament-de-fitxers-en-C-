0330e4ed76d4ed8e067cb2d02536b5b4ea130b74
JavierFernandezITB <146415434+JavierFernandezITB@users.noreply.github.com>
2024-04-07 20:33:24 +0200

    Add files via upload

---
+++ WorkspaceForHomework/Program.cs
@@ -0,0 +1,141 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+using System.Runtime.Intrinsics.X86;
+using System.Xml;
+using WorkspaceForHomework;
+
+namespace HomeworkWorkspaceNamespace
+{
+    public class HomeworkWorkspaceClass
+    {
+        
+        public static void Main()
+        {
+            const string SetCSVFileName = "Introduce el nombre de tu fichero CSV (default: ../../../CSV/ConsumAigua.csv): ";
+            const string ErrorMSG = "Error! ese fichero no existe o está vacío :(";
+            const string IncorrectQueryMSG = "Parece que el código o nombre es incorrecto.";
+            const string WelcomeMSG = "Bienvenido a mi programa de gestión de consumo de agua!";
+            const string XMLSaved = "Se ha creado un archivo XML con el contenido del CSV: ";
+            const string ChoiceMSG = "Que desea hacer?";
+            const string ExitMSG = "0. Salir";
+            const string IdentComarques = "1. Buscar comarcas con una población superior a 200.000";
+            const string CalConsDomestic = "2. Calcular el consum domèstic mitjà per comarca.";
+            const string CalcTopConsumers = "3. Mostrar les comarques amb el consum domèstic per càpita més alt. (Top 3)";
+            const string CalcLessConsumers = "4. Mostrar les comarques amb el consum domèstic per càpita més baix. (Top 3)";
+            const string FilterComarques = "5. Filtrar les comarques per nom o codi.";
+            const string AskCodName = "Introduce el codigo o nombre de la comarca.";
+            const string PressAnyKey = "Presiona cualquier tecla para continuar.";
+            const string csvDefaultFilepath = "../../../CSV/ConsumAigua.csv";
+            const int maxPopulation = 200000;
+
+            List<Consum> fetchedCSVResults = new List<Consum>(); 
+            string csvpath = "";
+            int choice;
+            bool EXIT = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write(SetCSVFileName);
+                    csvpath = Console.ReadLine();
+                    if (csvpath != "")
+                        fetchedCSVResults = Utils.ParseConsumCSV(csvpath);
+                    else
+                        fetchedCSVResults = Utils.ParseConsumCSV(csvDefaultFilepath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ErrorMSG);
+                }
+            } while (fetchedCSVResults.Count == 0);
+
+            Console.Clear();
+
+            do
+            {
+                Console.WriteLine(WelcomeMSG);
+                Console.WriteLine(XMLSaved + Utils.RecordsToXMLFile(fetchedCSVResults));
+                Console.WriteLine(ChoiceMSG);
+                Console.WriteLine(ExitMSG);
+                Console.WriteLine(IdentComarques);
+                Console.WriteLine(CalConsDomestic);
+                Console.WriteLine(CalcTopConsumers);
+                Console.WriteLine(CalcLessConsumers);
+                Console.WriteLine(FilterComarques);
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        EXIT = true;
+                        break;
+                    case 1:
+                        List<Consum> query = fetchedCSVResults.FindAll(x => x.Poblacio > maxPopulation);
+                        foreach (Consum consum in query)
+                        {
+                            Utils.PrettyPrint(consum.ToString());
+                        }
+                        break;
+                    case 2:
+                        List<int> codsComarcas = Utils.GetComarcas(fetchedCSVResults);
+                        foreach (int currentCod in codsComarcas)
+                        {
+                            float personas = 0;
+                            float totalCapita = 0;
+                            List<Consum> foundConsums = fetchedCSVResults.FindAll(x => x.CodComarca == currentCod);
+                            foreach (Consum consum in foundConsums)
+                            {
+                                totalCapita += consum.ConsumPerCapita;
+                                personas++;
+                            }
+                            float avg = totalCapita / personas;
+                            Utils.PrettyPrint($"Codi Comarca -> {currentCod} | Mitja: {avg}");
+                        }
+                        break;
+                    case 3:
+                        IEnumerable<Consum> topConsumer = fetchedCSVResults.OrderByDescending(c => c.ConsumPerCapita).Take(3);
+                        foreach (Consum current in topConsumer)
+                        {
+                            Utils.PrettyPrint(current.ToString());
+                        }
+                        break;
+                    case 4:
+                        IEnumerable<Consum> lessConsumer = fetchedCSVResults.OrderBy(c => c.ConsumPerCapita).Take(3);
+                        foreach (Consum current in lessConsumer)
+                        {
+                            Utils.PrettyPrint(current.ToString());
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine(AskCodName);
+                        string input = Console.ReadLine();
+                        var query1 = fetchedCSVResults.FindAll(x => x.CodComarca.ToString() == input);
+                        var query2 = fetchedCSVResults.FindAll(x => x.Comarca.ToString() == input);
+                        if (query1.Count != 0)
+                        {
+                            foreach (Consum current in query1)
+                            {
+                                Utils.PrettyPrint(current.ToString());
+                            }
+                        }
+                        else if (query2.Count != 0)
+                        {
+                            foreach (Consum current in query2)
+                            {
+                                Utils.PrettyPrint(current.ToString());
+                            }
+                        }
+                        else
+                            Utils.PrettyPrint(IncorrectQueryMSG);
+                        break;
+                }
+                Console.WriteLine(PressAnyKey);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            while (!EXIT);
+        }
+    }
+}
---
+++ WorkspaceForHomework/Utils.cs
@@ -0,0 +1,98 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WorkspaceForHomework
+{
+    public class Utils
+    {
+        const string prettySeparator = "+---------------------------------------------------------------------------------";
+        const string mainPath = "../../../XML/ConsumAiguaRecords";
+
+        public static List<Consum> ParseConsumCSV(string filepath)
+        {
+            List<Consum> resultList = new List<Consum>();
+            using StreamReader reader = new StreamReader(filepath);
+            using CsvReader csvreader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csvreader.Read();
+            csvreader.ReadHeader();
+            while (csvreader.Read())
+            {
+                var record = new Consum
+                {
+                    Any = csvreader.GetField<int>("Any"),
+                    CodComarca = csvreader.GetField<int>("Codi comarca"),
+                    Comarca = csvreader.GetField<string>("Comarca"),
+                    Poblacio = csvreader.GetField<int>("Població"),
+                    XarxaDomestica = csvreader.GetField<int>("Domèstic xarxa"),
+                    ActivitatsEconomiques = csvreader.GetField<int>("Activitats econòmiques i fonts pròpies"),
+                    Total = csvreader.GetField<int>("Total"),
+                    ConsumPerCapita = csvreader.GetField<float>("Consum domèstic per càpita")
+                };
+                resultList.Add(record);
+            }
+            return resultList;
+        }
+
+        public static string RecordsToXMLFile(List<Consum> recordsList)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "  ",
+                NewLineChars = "\r\n",
+                NewLineHandling = NewLineHandling.Replace
+            };
+            DateTime dateNow = DateTime.Now;
+            string formattedDate = dateNow.ToString("ddMMyyyy_hhmmss");
+            string filepath = mainPath + $"{formattedDate}.xml";
+            using (XmlWriter output = XmlWriter.Create(filepath, settings))
+            {
+                output.WriteStartDocument();
+
+                output.WriteStartElement("Records");
+                foreach (Consum record in recordsList)
+                {
+                    output.WriteStartElement("Record");
+                    output.WriteElementString("Any", record.Any.ToString());
+                    output.WriteElementString("CodComarca", record.CodComarca.ToString());
+                    output.WriteElementString("Comarca", record.Comarca.ToString());
+                    output.WriteElementString("Poblacio", record.Poblacio.ToString());
+                    output.WriteElementString("XarxaDomestica", record.XarxaDomestica.ToString());
+                    output.WriteElementString("ActivitatsEconomiques", record.ActivitatsEconomiques.ToString());
+                    output.WriteElementString("Total", record.Total.ToString());
+                    output.WriteElementString("ConsumPerCapita", record.ConsumPerCapita.ToString());
+                    output.WriteEndElement();
+                }
+                output.WriteEndElement();
+            }
+            return filepath;
+        }
+
+        public static void PrettyPrint(string text)
+        {
+            Console.WriteLine(prettySeparator);
+            Console.WriteLine("| -> " + text);
+            Console.WriteLine(prettySeparator);
+        }
+
+        public static List<int> GetComarcas(List<Consum> recordsList)
+        {
+            List<int> Comarcas = new List<int>();
+            foreach (Consum current in recordsList)
+            {
+                if (Comarcas.Find(x => current.CodComarca == x) == 0)
+                {
+                    Comarcas.Add(current.CodComarca);
+                }
+            }
+            return Comarcas;
+        }
+
+    }
+}
